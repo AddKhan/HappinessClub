edd8c1f3b8b17d4925b5f7f448927bc71836146d
AddKhan <adrinakhan2006@gmail.com>
2025-07-27 09:05:53 -0400

    updated challenge list, began generation function

---
+++ script.js
@@ -0,0 +1,26 @@
+// 7/27/25
+// daily-challenges.html
+
+let myScore = localStorage.getItem('myScore') || 0; // begining score is 0
+const challengeList = [
+    `Go for a walk`,
+    `Reflect on your day`,
+    `Compliment a stranger`,
+    `Make a new friend`,
+    `Donate an item`,
+    `Volunteer`,
+    `Call someone you haven't talked to in a while`,
+    `Help a stranger`,
+    `Try a new food`,
+    `Catch up with old friends`,
+    `placeholderAction1`,
+    `placeholderAction2`,
+    `placeholderAction3`,
+    `placeholderAction4`,
+    `placeholderAction5`,
+]
+
+function generateChallenges(){
+    let shuffledChallengeList = challengeList.sort(() => Math.random() - 0.5); // shuffle
+    const selectedChallengeList = shuffledChallengeList.slice(0, 3); // choose first 3 in shuffled list
+}
